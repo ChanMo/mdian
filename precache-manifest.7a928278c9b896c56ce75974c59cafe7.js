66229658f671971526762943abda0400c03cf773
dyou <chen.orange@aliyun.com>
2019-07-18 18:07:07 +0800

    Updates

---
+++ precache-manifest.7a928278c9b896c56ce75974c59cafe7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf0c7677c91dcd59605bebcdaa06879",
+    "url": "/mdian/index.html"
+  },
+  {
+    "revision": "4e228d201dc0199fd5d6",
+    "url": "/mdian/static/css/main.c331a694.chunk.css"
+  },
+  {
+    "revision": "e7dba32d7a162cda4865",
+    "url": "/mdian/static/js/2.2340f3b4.chunk.js"
+  },
+  {
+    "revision": "4e228d201dc0199fd5d6",
+    "url": "/mdian/static/js/main.067e09de.chunk.js"
+  },
+  {
+    "revision": "d750bf7a9e23ccb868cd",
+    "url": "/mdian/static/js/runtime~main.2f0fb5f4.js"
+  },
+  {
+    "revision": "92b8a3236363a5d5db7001622a09e24c",
+    "url": "/mdian/static/media/01.92b8a323.png"
+  },
+  {
+    "revision": "bc457088ff950e9ec8390cec667be130",
+    "url": "/mdian/static/media/02.bc457088.png"
+  },
+  {
+    "revision": "bbbb471240c83d9fb7eee854aa57c879",
+    "url": "/mdian/static/media/03.bbbb4712.png"
+  },
+  {
+    "revision": "dfd997e4d6762237963779e19e17bfab",
+    "url": "/mdian/static/media/04.dfd997e4.png"
+  },
+  {
+    "revision": "b62d4cd61884c3c697301d24e87e4b12",
+    "url": "/mdian/static/media/aboutus.b62d4cd6.png"
+  },
+  {
+    "revision": "d31364378b6539998b7cdf8fde92e77e",
+    "url": "/mdian/static/media/bg.d3136437.png"
+  },
+  {
+    "revision": "fe40aa47c672e9d8c44c30d216d3dc39",
+    "url": "/mdian/static/media/huobip.fe40aa47.png"
+  },
+  {
+    "revision": "d308f4c7458c84fca5a252e51e71853b",
+    "url": "/mdian/static/media/qrcode.d308f4c7.png"
+  },
+  {
+    "revision": "0a49ff4e055960552ba54239ca9f7164",
+    "url": "/mdian/static/media/xys19.0a49ff4e.png"
+  }
+]);
