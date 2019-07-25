a9937a76d9eac2a9f0e66ceb9755e1b52bcd5269
dyou <chen.orange@aliyun.com>
2019-07-25 09:59:59 +0800

    add news

--- src/News.js
+++ src/News.js
@@ -3,10 +3,12 @@ import styled from 'styled-components'
 import Section from './Section'
 
 const list = [
+  [288, '租房痛点如何解决？来看看区块链技术', 'http://image.woshipm.com/wp-files/2018/08/7GFD9p3xF3GE9Kzn3hvS.jpg!v.jpg', '在租房的问题上，很多人或多或少都遇到过一些问题，那针对于这些问题，要怎样更好的去解决呢？', '2019.07.25'],
   [192, '从产品角度，看小程序的设计', 'https://cms.mandiankeji.com/wp-content/uploads/2019/07/rdn_512abfa7f0873.jpg', '6年多的时间，帮客户设计的小程序，大大小小几十个，有些运营的很好，有些却水土不服，今天就来聊聊小程序的设计思考', '2019.05.14']
 ]
 
 const Item = styled.div`
+  margin-bottom: 1rem;
   display: flex;
   align-items: flex-start;
   img {
