1290f18681eeea67d8ec060d901410b957bdcc4c
dyou <chen.orange@aliyun.com>
2019-08-31 17:52:31 +0800

    remove deploy files

--- fabfile.py
+++
@@ -1,9 +0,0 @@
-from fabric import task
-
-
-@task
-def deploy(c):
-    c.put("build.tar.gz", "/var/www/mdian_com/")
-    with c.cd("/var/www/mdian_com/"):
-        c.run("tar zxf build.tar.gz")
-        c.run("rm build.tar.gz")
