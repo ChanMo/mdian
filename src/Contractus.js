5808ace141adee8f42aece2725ebcea230dbbe12
dyou <chen.orange@aliyun.com>
2019-08-31 17:50:19 +0800

    add link

--- src/Contractus.js
+++ src/Contractus.js
@@ -17,6 +17,10 @@ const Grid = styled.div`
   p {
     color: white;
   }
+  a {
+    color: white;
+    margin-right: .5rem;
+  }
   span {
     margin-right: .5rem;
   }
@@ -41,10 +45,10 @@ export default function Contractus() {
       <Grid>
         <img src={require("./qrcode.png")} alt="qrcode" />
         <div>
-          <p><span>mandiankeji@163.com</span><span>邮箱</span></p>
-          <p><span>13145312751</span><span>电话</span></p>
+          <p><a href="mailto:mandiankeji@163.com">mandiankeji@163.com</a><span>邮箱</span></p>
+          <p><a href="tel://13145312751">13145312751</a><span>电话</span></p>
           <p><span>mandian1927</span><span>微信</span></p>
-          <p><span>高新区汇东国际1号楼</span><span>地址</span></p>
+          <p><a href="https://uri.amap.com/marker?position=117.10966,36.694592">山东济南高新区汇东国际1号楼</a><span>地址</span></p>
         </div>
       </Grid>
     </Section>
